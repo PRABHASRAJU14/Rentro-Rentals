c014994abdd2a9dc58a00fcef9f95fb6b77e95d5
Gnanada <alavala.gnanada.542@gmail.com>
2025-05-30 20:41:48 +0530

    done

--- scripts/start.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('consumerBtn').addEventListener('click', () => {
-  window.location.href = 'html/start.html';
-});
-
-document.getElementById('providerBtn').addEventListener('click', () => {
-  window.location.href = 'html/dashboard.html';
-});
